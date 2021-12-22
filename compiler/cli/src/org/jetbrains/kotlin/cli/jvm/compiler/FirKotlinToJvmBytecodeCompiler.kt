e43a31bc9e8c930033f618258534512d9fb370f1
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2021-12-22 16:07:42 +0100

    ~ IC logic in tight loop partly works

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/FirKotlinToJvmBytecodeCompiler.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/FirKotlinToJvmBytecodeCompiler.kt
@@ -184,7 +184,7 @@ object FirKotlinToJvmBytecodeCompiler {
 
         val providerAndScopeForIncrementalCompilation = createComponentsForIncrementalCompilation(sourceScope)
 
-        providerAndScopeForIncrementalCompilation?.scope?.let {
+        providerAndScopeForIncrementalCompilation?.precompiledBinariesFileScope?.let {
             librariesScope -= it
         }
 
@@ -268,7 +268,7 @@ object FirKotlinToJvmBytecodeCompiler {
 
     private fun CompilationContext.createComponentsForIncrementalCompilation(
         sourceScope: AbstractProjectFileSearchScope
-    ): FirSessionFactory.ProviderAndScopeForIncrementalCompilation? {
+    ): FirSessionFactory.IncrementalCompilationContext? {
         if (targetIds == null || incrementalComponents == null) return null
         val directoryWithIncrementalPartsFromPreviousCompilation =
             moduleConfiguration[JVMConfigurationKeys.OUTPUT_DIRECTORY]
@@ -282,7 +282,7 @@ object FirKotlinToJvmBytecodeCompiler {
             projectEnvironment.getPackagePartProvider(sourceScope),
             targetIds.map(incrementalComponents::getIncrementalCache)
         )
-        return FirSessionFactory.ProviderAndScopeForIncrementalCompilation(packagePartProvider, incrementalCompilationScope)
+        return FirSessionFactory.IncrementalCompilationContext(emptyList(), packagePartProvider, incrementalCompilationScope)
     }
 
     private fun CompilationContext.runBackend(
