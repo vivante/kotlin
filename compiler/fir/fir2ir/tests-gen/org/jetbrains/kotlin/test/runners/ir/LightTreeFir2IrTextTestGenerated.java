7ebe036ff166191bec95c67b2d92d62c51b6534b
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-01-24 14:02:43 +0100

    ~ fixup missing test

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/LightTreeFir2IrTextTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/LightTreeFir2IrTextTestGenerated.java
@@ -2543,6 +2543,12 @@ public class LightTreeFir2IrTextTestGenerated extends AbstractLightTreeFir2IrTex
         }
 
         @Test
+        @TestMetadata("lambdaInEnumEntryConstructorCall.kt")
+        public void testLambdaInEnumEntryConstructorCall() throws Exception {
+            runTest("compiler/testData/ir/irText/firProblems/lambdaInEnumEntryConstructorCall.kt");
+        }
+
+        @Test
         @TestMetadata("localClassUsedBeforeDeclaration.kt")
         public void testLocalClassUsedBeforeDeclaration() throws Exception {
             runTest("compiler/testData/ir/irText/firProblems/localClassUsedBeforeDeclaration.kt");
